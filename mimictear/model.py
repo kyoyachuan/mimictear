278ad8c3b4d4b324b1c50292aa545e4a1451a8d4
kyoyachuan <kyoyachuan0626@gmail.com>
2022-08-16 13:31:33 +0800

    refactor: add spectral mormalization and some code changes

--- mimictear/model.py
+++ mimictear/model.py
@@ -19,6 +19,62 @@ def init_conv(conv):
     return conv
 
 
+class SpectralNorm:
+    def __init__(self, name):
+        self.name = name
+
+    def compute_weight(self, module):
+        weight = getattr(module, self.name + '_orig')
+        u = getattr(module, self.name + '_u')
+        size = weight.size()
+        weight_mat = weight.contiguous().view(size[0], -1)
+        with torch.no_grad():
+            v = weight_mat.t() @ u
+            v = v / v.norm()
+            u = weight_mat @ v
+            u = u / u.norm()
+        sigma = u @ weight_mat @ v
+        weight_sn = weight / sigma
+        # weight_sn = weight_sn.view(*size)
+
+        return weight_sn, u
+
+    @staticmethod
+    def apply(module, name):
+        fn = SpectralNorm(name)
+
+        weight = getattr(module, name)
+        del module._parameters[name]
+        module.register_parameter(name + '_orig', weight)
+        input_size = weight.size(0)
+        u = weight.new_empty(input_size).normal_()
+        module.register_buffer(name, weight)
+        module.register_buffer(name + '_u', u)
+
+        module.register_forward_pre_hook(fn)
+
+        return fn
+
+    def __call__(self, module, input):
+        weight_sn, u = self.compute_weight(module)
+        setattr(module, self.name, weight_sn)
+        setattr(module, self.name + '_u', u)
+
+
+def spectral_norm(module, name='weight'):
+    SpectralNorm.apply(module, name)
+
+    return module
+
+
+def spectral_init(module, gain=1):
+    init.xavier_uniform_(module.weight, gain)
+    if module.bias is not None:
+        module.bias.data.zero_()
+
+    return spectral_norm(module)
+
+
 def leaky_relu(input):
     return F.leaky_relu(input, negative_slope=0.2)
 
@@ -27,9 +83,9 @@ class SelfAttention(nn.Module):
     def __init__(self, in_channel):
         super().__init__()
 
-        self.query = init_conv(nn.Conv1d(in_channel, in_channel // 8, 1))
-        self.key = init_conv(nn.Conv1d(in_channel, in_channel // 8, 1))
-        self.value = init_conv(nn.Conv1d(in_channel, in_channel, 1))
+        self.query = spectral_init(nn.Conv1d(in_channel, in_channel // 8, 1))
+        self.key = spectral_init(nn.Conv1d(in_channel, in_channel // 8, 1))
+        self.value = spectral_init(nn.Conv1d(in_channel, in_channel, 1))
 
         self.gamma = nn.Parameter(torch.tensor(0.0))
 
@@ -53,8 +109,8 @@ class ConditionalNorm(nn.Module):
         super().__init__()
 
         self.bn = nn.BatchNorm2d(in_channel, affine=False)
-        self.embed_g = init_linear(nn.Linear(n_class, in_channel, bias=False))
-        self.embed_b = init_linear(nn.Linear(n_class, in_channel, bias=False))
+        self.embed_g = spectral_init(nn.Linear(n_class, in_channel, bias=False))
+        self.embed_b = spectral_init(nn.Linear(n_class, in_channel, bias=False))
 
     def forward(self, input, cond):
         out = self.bn(input)
@@ -85,11 +141,11 @@ class ConvBlock(nn.Module):
 
         self.deconv = deconv
         if deconv:
-            self.conv = init_conv(nn.ConvTranspose2d(
+            self.conv = spectral_init(nn.ConvTranspose2d(
                 in_channel, out_channel, kernel_size, stride, padding, bias=False
             ))
         else:
-            self.conv = init_conv(nn.Conv2d(
+            self.conv = spectral_init(nn.Conv2d(
                 in_channel, out_channel, kernel_size, stride, padding, bias=False
             ))
 
@@ -97,8 +153,6 @@ class ConvBlock(nn.Module):
         self.cbn = cbn
         if cbn:
             self.norm = ConditionalNorm(out_channel, n_class)
-        else:
-            self.norm = nn.BatchNorm2d(out_channel)
 
         self.self_attention = self_attention
         if self_attention:
@@ -110,8 +164,6 @@ class ConvBlock(nn.Module):
 
         if self.cbn:
             out = self.norm(out, cond)
-        else:
-            out = self.norm(out)
 
         if self.activation is not None:
             out = self.activation(out)
@@ -131,10 +183,10 @@ class Generator(nn.Module):
         self.cbn = cbn
 
         if not cbn:
-            self.cond_embed = init_linear(nn.Linear(n_class, cond_dim, bias=False))
+            self.cond_embed = spectral_init(nn.Linear(n_class, cond_dim, bias=False))
         cond_dim = cond_dim if not cbn else 0
 
-        self.lin_code = init_linear(nn.Linear(code_dim, 1 * 1 * 512))
+        self.lin_code = spectral_init(nn.Linear(code_dim, 1 * 1 * 512))
         self.conv = nn.ModuleList(
             [
                 ConvBlock(512 + cond_dim, 512, padding=0, deconv=True, n_class=n_class, cbn=cbn),
@@ -144,7 +196,7 @@ class Generator(nn.Module):
             ]
         )
 
-        self.colorize = init_conv(nn.ConvTranspose2d(64, 3, 4, stride=2, padding=1))
+        self.colorize = spectral_init(nn.ConvTranspose2d(64, 3, 4, stride=2, padding=1))
 
     def forward(self, input, cond=None):
         out = torch.relu(self.lin_code(input))
@@ -170,10 +222,10 @@ class Discriminator(nn.Module):
         self.input_dim = self.C
 
         if not projection:
-            self.cond_embed = init_linear(nn.Linear(n_class, self.C * self.H * self.W, bias=False))
+            self.cond_embed = spectral_init(nn.Linear(n_class, self.C * self.H * self.W, bias=False))
             self.input_dim *= 2
         else:
-            self.cond_embed = init_linear(nn.Linear(n_class, 512, bias=False))
+            self.cond_embed = spectral_init(nn.Linear(n_class, 512, bias=False))
 
         self.conv = nn.Sequential(ConvBlock(self.input_dim, 64, activation=leaky_relu),
                                   ConvBlock(64, 128, activation=leaky_relu),
@@ -181,7 +233,7 @@ class Discriminator(nn.Module):
                                   ConvBlock(256, 512, activation=leaky_relu),
                                   ConvBlock(512, 512, activation=leaky_relu))
 
-        self.linear = init_linear(nn.Linear(512, 1))
+        self.linear = spectral_init(nn.Linear(512, 1))
 
     def forward(self, input, cond):
         if not self.projection:
